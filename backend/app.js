01c60125cba5cfc2e32ac6625d1d0aea38032cf8
XieLiang <115782412+Aspartame6888@users.noreply.github.com>
2024-06-13 18:21:12 +0800

    feature: Add oppoTestApi to post data

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,86 @@
-const express = require('express');
-const axios = require('axios');
-const schedule = require('node-schedule');
-const logger = require('./config/logger');
-const bodyParser = require('body-parser');
-const dataRoutes = require('./routes/data');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require("express");
+const axios = require("axios");
+const schedule = require("node-schedule");
+const logger = require("./config/logger");
+const bodyParser = require("body-parser");
+const dataRoutes = require("./routes/data");
+const cors = require("cors");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const CryptoJS = require("crypto-js");
 
 const app = express();
 const PORT = process.env.PORT || 5008;
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-// app.use('/api/data', dataRoutes);
 app.use(bodyParser.json());
-app.use('/api', dataRoutes);
+app.use("/api", dataRoutes);
 
-const thirdPartyApi1 = 'https://api.example.com/data';
-const thirdPartyApi2 = 'https://api.example2.com/submit';
+const thirdPartyApi1 = "https://api.example.com/data";
+const oppoTestApi =
+  "https://global-search-sg-test.wanyol.com/gis/open/income/add";
 
+// 定时任务：每三分钟一次 for test
+// schedule.scheduleJob("*/3 * * * *", async () => {
 // 定时任务：每天一次
-schedule.scheduleJob('0 0 * * *', async () => {
+schedule.scheduleJob("0 0 * * *", async () => {
   try {
-    const response = await axios.get(thirdPartyApi1);
-    const data = response.data;
-    
-    await axios.post(thirdPartyApi2, data);
-    logger.info('Data sent successfully.');
+    // const response = await axios.get(thirdPartyApi1);
+    // const data = response.data;
+    const data = {
+      incomeList: [
+        {
+          date: 20240301,
+          brand: "OPPO",
+          model: "PGEM",
+          region: "TH",
+          customId: "Google",
+          media: "商店",
+          currency: "USD",
+          income: 123450000,
+          incomeType: "",
+          customApp: "",
+          impressions: 0,
+        },
+      ],
+    };
+
+    const apiKey = "89fc80d5a056d8d69e18b4648fd32bd0";
+    const apiSecret =
+      "Zh5J9Blwkl64uQ6GGXSb2ET25Ruwkk269slGSaDuRYd/ySx5TA+bBQ==";
+    const timestamp = Date.now().toString();
+    const sign = CryptoJS.SHA256(apiKey + apiSecret + timestamp).toString(
+      CryptoJS.enc.Hex
+    );
+
+    const headers = {
+      Accept: "application/json",
+      timestamp,
+      apiKey,
+      sign,
+    };
+
+    const response = await axios.post(oppoTestApi, data, { headers });
+    const successMessage = `Success: Data sent successfully at ${new Date().toISOString()}. Response: ${JSON.stringify(
+      response.data
+    )}`;
+    logger.info(successMessage);
+    fs.appendFileSync(
+      path.join(__dirname, "logs", "success.log"),
+      successMessage + "\n"
+    );
   } catch (error) {
-    logger.error(`Error: ${error.message}`);
+    const errorMessage = `Error: ${
+      error.message
+    } at ${new Date().toISOString()}.`;
+    logger.error(errorMessage);
+    fs.appendFileSync(
+      path.join(__dirname, "logs", "error.log"),
+      errorMessage + "\n"
+    );
   }
 });
 
--- backend/routes/data.js
+++ backend/routes/data.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-router.get('/income', (req, res) => {
-  const filePath = path.join(__dirname, '../data/incomeData.json');
-  fs.readFile(filePath, 'utf8', (err, data) => {
+router.get("/income", (req, res) => {
+  const filePath = path.join(__dirname, "../data/incomeData.json");
+  fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
-      console.error('Error reading file:', err);
-      res.status(500).json({ code: 1001, msg: 'internal error', success: false });
+      console.error("Error reading file:", err);
+      res
+        .status(500)
+        .json({ code: 1001, msg: "internal error", success: false });
       return;
     }
     res.json(JSON.parse(data));
