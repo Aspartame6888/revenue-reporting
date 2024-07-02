c418fda05da91889b3a4af8daefaab7376e203de
XieLiang <115782412+Aspartame6888@users.noreply.github.com>
2024-07-02 21:01:44 +0800

    feature： Add config

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,8 +3,11 @@ module.exports = {
   clientId: "900a288f7bb24948b9dc2fbf7881ceec",
   clientSecret: "637265a785394c439cf9a5c11f2681d6",
   oppoTestApi: "https://global-search-sg-test.wanyol.com/gis/open/income/add",
-  apiKey: "89fc80d5a056d8d69e18b4648fd32bd0",
-  apiSecret: "Zh5J9Blwkl64uQ6GGXSb2ET25Ruwkk269slGSaDuRYd/ySx5TA+bBQ==",
+  oppoPrdApi: "https://api-gl.open.heytapmobile.com/gis/open/income/add",
+  testApiKey: "89fc80d5a056d8d69e18b4648fd32bd0",
+  testApiSecret: "Zh5J9Blwkl64uQ6GGXSb2ET25Ruwkk269slGSaDuRYd/ySx5TA+bBQ==",
+  prdApiKey: "6c622e322d8f7a87228cf36d5d3e1226",
+  prdApiSecret: "/kd7+8A+F7gnm+SeQgrDY7i98o30ggccjzrr1zBLYS9/ySx5TA+bBQ==",
 
   bsApi: (() => {
     const baseUrl = "https://backstage.taboola.com/backstage/api/1.0";
@@ -79,7 +82,7 @@ module.exports = {
       "oppo-browser-spanish-fallback",
       "oppo-browser-italian-fallback",
       "oppo-browser-indonesia",
-      "oppo-superapp-fallback-eng",
+      // "oppo-superapp-fallback-eng",
       "oppo-browser-philippine-push-feed",
       "oppo-browser-malaysia-push-feed",
       "oppo-lockscreen-is",
