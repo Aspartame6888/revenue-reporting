259d6aa2634a8d459270d170887082b0befc7f60
XieLiang <115782412+Aspartame6888@users.noreply.github.com>
2024-07-29 18:37:02 +0800

    feature: Add database retry connection

--- backend/services/db.js
+++ backend/services/db.js
@@ -1,19 +1,38 @@
 const mysql = require("mysql2/promise");
 const config = require("../config/config");
 
-let connection;
+let connection = null;
 
 const connectDB = async () => {
-  if (!connection) {
-    connection = await mysql.createConnection({
-      host: config.dbHost,
-      port: config.dbPort,
-      user: config.dbUser,
-      password: config.dbPassword,
-    //   database: config.dbName,
-    });
+  let retries = 0;
+  const maxRetries = 5;
+  while (!connection && retries < maxRetries) {
+    try {
+      connection = await mysql.createConnection({
+        host: config.dbHost,
+        port: config.dbPort,
+        user: config.dbUser,
+        password: config.dbPassword,
+        // database: config.dbName,
+      });
 
-    console.log("Connected to the database.");
+      // console.log("Connected to the database.");
+    } catch (error) {
+      retries += 1;
+      console.error(
+        `Failed to connect to the database (Attempt ${retries}/${maxRetries}): ${error.message}`
+      );
+
+      if (retries >= maxRetries) {
+        console.error(
+          "Max retries reached. Could not connect to the database."
+        );
+        throw new Error("Could not connect to the database.");
+      } else {
+        console.log(`Retrying to connect in ${retries * 2} seconds...`);
+        await new Promise((resolve) => setTimeout(resolve, retries * 2000));
+      }
+    }
   }
   return connection;
 };
@@ -21,13 +40,16 @@ const connectDB = async () => {
 const getStatus = async (name) => {
   try {
     const connection = await connectDB();
+    if (!connection) {
+      return false;
+    }
     const [rows] = await connection.query(
       "SELECT status FROM trc.publishers WHERE name = ?",
       [name]
     );
     return rows[0]?.status === "LIVE";
   } catch (error) {
-    console.error(`Failed to get status for ${name}: ${error.message}`);
+    // console.error(`Failed to get status for ${name}: ${error.message}`);
     return false;
   }
 };
